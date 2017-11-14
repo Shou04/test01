3b635097a53fdbb5ab0391a2f253be1c92507384
ShouWatanabe <hoshishou00@gmail.com>
2017-11-14 16:51:52 +0900

    test.shの変更

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 javac TestProgram.java -encoding utf8
 java TestProgram ver01 test01 abcdefghijklmnopqrstu
-java TestProgram ver01 test02 abcdefghijkl
-java TestProgram ver01 test03 abcdefghijklnopqrstu
-java TestProgram ver01 test04 abcdnopqrstu
-java TestProgram ver01 test05 aijklmnopqrstu
+java TestProgram ver02 test02 abcdefghijkl
+java TestProgram ver03 test03 abcdefghijklnopqrstu
+java TestProgram ver04 test04 abcdnopqrstu
+java TestProgram ver05 test05 aijklmnopqrstu

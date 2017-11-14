8bcf2ce274d73640f6e74d3b7d9cee3650911e5c
ShouWatanabe <hoshishou00@gmail.com>
2017-11-14 16:46:00 +0900

    kjhkjakajh

--- TestProgram.java
+++ TestProgram.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.ZonedDateTime;
 
 
 public class TestProgram{
@@ -9,15 +10,14 @@ public class TestProgram{
         int    x = 7;
         int    y = 3;
         String z = null;
+        String time = null;
         String filename = null;
         String dirname  = null;
-
         MakeDir makeD = new MakeDir();
+        ZonedDateTime zd = ZonedDateTime.now();
 
         dirname  = "c:/jenkins_plugin_workspase/Data/" + args[0];
         filename = dirname+ "/" + args[1] + ".txt";
-
-        //filename = "c:/jenkins_plugin_workspase/test/data_test.txt";
 
         makeD.makeDir(dirname); //新しいディレクトリを作成
 
@@ -31,10 +31,11 @@ public class TestProgram{
 
           if (checkBeforeWritefile(file)){
             FileWriter filewriter = new FileWriter(file, true);
-            filename = args[0]+".txt" + "\n";
-            z        = String.valueOf(args[2])+"\n";
 
-            filewriter.write(filename);
+            z    = String.valueOf(args[2])+"\n";
+            time = String.valueOf(zd)+"\n";
+
+            filewriter.write(time);
             filewriter.write(z);
 
             filewriter.close();
@@ -44,7 +45,7 @@ public class TestProgram{
         }catch(IOException e){
           System.out.println(e);
         }
-        
+
     }
 
     private static boolean checkBeforeWritefile(File file){
---
+++ test.sh
@@ -0,0 +1,6 @@
+javac TestProgram.java -encoding utf8
+java TestProgram ver01 test01 abcdefghijklmnopqrstu
+java TestProgram ver01 test02 abcdefghijkl
+java TestProgram ver01 test03 abcdefghijklnopqrstu
+java TestProgram ver01 test04 abcdnopqrstu
+java TestProgram ver01 test05 aijklmnopqrstu
